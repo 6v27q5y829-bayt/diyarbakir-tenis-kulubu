19b8f0b8c3b4919d6841f127d3e23dbeb6e86211
6v27q5y829-bayt <6v27q5y829@privaterelay.appleid.com>
2025-12-08 13:54:21 +0300

    Added firebase.js

---
+++ firebase.js
@@ -0,0 +1,34 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore, collection, doc, setDoc, getDoc, getDocs, updateDoc, deleteDoc, onSnapshot, query, where } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsOde5aNv6HnNJMXtPLlGisXF_zXp3E5o",
+  authDomain: "diyarbakirtenis-254b1.firebaseapp.com",
+  projectId: "diyarbakirtenis-254b1",
+  storageBucket: "diyarbakirtenis-254b1.firebasestorage.app",
+  messagingSenderId: "664290250808",
+  appId: "1:664290250808:web:bd0ff27083ea50f145d6cd",
+  measurementId: "G-LL1R6KD89Q"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  collection,
+  doc,
+  setDoc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  onSnapshot,
+  query,
+  where
+};
